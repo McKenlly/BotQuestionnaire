8ea7c7ee698931e61c964f80540c3a78405eebc3
Seriy <bokoch98@yandex.ru>
2018-04-11 00:21:33 +0600

    First Steps

---
+++ LoftBlog_TrainingBot/Commands/Questions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using LoftBlog_TrainingBot.Interfaces;
+using LoftBlog_TrainingBot.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+
+namespace LoftBlog_TrainingBot.Commands
+{
+    [Serializable]
+    public class Questions : ITool
+    {
+        public string Description { get; set; }
+        public List<string> CommandsName { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public Questions()
+        {
+            Description = "Викторина для квеста ImagineCup";
+            CommandsName = new List<string>() { "/questions" };
+        }
+
+        public virtual async Task Run(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var activity = await result as Activity;
+            if (activity?.Conversation != null)
+            {
+                await context.Forward(MakeInterviewDialog(), ResumeAfterNewDialog, activity, CancellationToken.None);
+            }
+        }
+
+
+        internal static IDialog<InterviewInfo> MakeInterviewDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(InterviewInfo.BuildForm));
+        }
+
+        private async Task ResumeAfterNewDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            var answers = await result as InterviewInfo;
+            if (answers != null)
+            {
+                await context.PostAsync("Ваши данные успешно получены. Спасибо!");
+                context.Done(result);
+            }
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(Run);
+        }
+
+    }
+}
---
+++ LoftBlog_TrainingBot/Models/InterviewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using LoftBlog_TrainingBot.Attributes;
+
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace LoftBlog_TrainingBot.Models
+{
+    public enum QuestionsOneOptions
+    {
+        One = 2,
+        Two = 4,
+        Three = 6
+    };
+    public enum QuestionsTwoOptions
+    {
+        One,
+        Two, 
+        Three,
+        Four
+    };
+    public enum QuestionsThreeOptions
+    {
+        One,
+        Two,
+        Three,
+        Four
+    };
+    public enum QuestionsFourOptions
+    {
+        Один,
+        Два,
+        Три,
+        Четыре
+     };
+
+
+    [Serializable]
+    public class InterviewInfo
+    {
+        [LocalizableDescribe("QuestionOne", typeof(Resources.Resources))]
+        public QuestionsOneOptions? One;
+
+        [LocalizableDescribe("QuestionTwo", typeof(Resources.Resources))]
+        public QuestionsTwoOptions? Two;
+
+        [LocalizableDescribe("QuestionThree", typeof(Resources.Resources))]
+        public QuestionsThreeOptions? Three;
+
+        [LocalizableDescribe("QuestionFour", typeof(Resources.Resources))]
+        public QuestionsFourOptions? Four;
+
+
+        public static IForm<InterviewInfo> BuildForm()
+        {
+            return new FormBuilder<InterviewInfo>().Build();
+        }
+    };
+}
